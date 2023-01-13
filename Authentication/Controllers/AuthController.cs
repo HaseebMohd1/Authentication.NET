71631bc0e88d161b7e9df8c4acf5dae44eedf0b9
Haseeb Mohd <haseeb.m@Technovert.com>
2023-01-13 11:02:21 +0530

    Added Refresh Token

--- Authentication/Controllers/AuthController.cs
+++ Authentication/Controllers/AuthController.cs
@@ -46,6 +46,8 @@ namespace Authentication.Controllers
         {
             CreatePasswordHash(userDetails.Password, out byte[] passwordSalt, out byte[] passwordHash);
 
+            
+
             user.UserName = userDetails.UserName;
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
@@ -67,6 +69,32 @@ namespace Authentication.Controllers
             }
 
             string token = CreateToken(user);
+
+            RefreshToken refreshToken = GenerateRefreshToken();
+
+            SetRefreshToken(refreshToken);
+
+            return Ok(token);
+        }
+
+
+        [HttpPost("refresh-token")]
+        public async Task<ActionResult<string>> RefreshToken()
+        {
+            var refreshToken = Request.Cookies["refreshToken"];
+
+            if (!user.RefreshToken.Equals(refreshToken))
+            {
+                return Unauthorized("Invalid Refresh Token.");
+            }
+            else if (user.TokenExpires < DateTime.Now)
+            {
+                return Unauthorized("Token expired.");
+            }
+
+            string token = CreateToken(user);
+            var newRefreshToken = GenerateRefreshToken();
+            SetRefreshToken(newRefreshToken);
 
             return Ok(token);
         }
@@ -113,6 +141,32 @@ namespace Authentication.Controllers
 
                 return computedHash.SequenceEqual(passwordHash);
             }
+        }
+
+        private RefreshToken GenerateRefreshToken()
+        {
+            var refreshToken = new RefreshToken
+            {
+                Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+                Expires = DateTime.Now.AddDays(7),
+                Created = DateTime.Now
+            };
+
+            return refreshToken;
+        }
+
+        private void SetRefreshToken(RefreshToken newRefreshToken)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Expires = newRefreshToken.Expires
+            };
+            Response.Cookies.Append("refreshToken", newRefreshToken.Token, cookieOptions);
+
+            user.RefreshToken = newRefreshToken.Token;
+            user.TokenCreated = newRefreshToken.Created;
+            user.TokenExpires = newRefreshToken.Expires;
         }
     }
 }
